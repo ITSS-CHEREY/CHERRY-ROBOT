062a8ba6f565b2443c48a0a40d90bdddcecbf83a
Cɦεɾɾy <133900927+ITSS-CHEREY@users.noreply.github.com>
2023-05-20 14:47:40 +0530

    Update animez.py

--- MukeshRobot/modules/animez.py
+++ MukeshRobot/modules/animez.py
@@ -554,7 +554,7 @@ __help__ = """
  ❍ /kayo <ᴀɴɪᴍᴇ>*:* sᴇᴀʀᴄʜ ᴀɴ ᴀɴɪᴍᴇ ᴏɴ ᴀɴɪᴍᴇᴋᴀʏᴏ.ᴄᴏᴍ
  ❍ /airing <ᴀɴɪᴍᴇ>*:* ʀᴇᴛᴜʀɴs ᴀɴɪᴍᴇ ᴀɪʀɪɴɢ ɪɴғᴏ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [CHERRY](https://t.me/DRACULA_CHEERY)............☆
 """
 
 ANIME_HANDLER = DisableAbleCommandHandler("anime", anime)
