6b5ff8dfc048ea1286c0b68ee869731ee0f87bd5
Cɦεɾɾy <133900927+ITSS-CHEREY@users.noreply.github.com>
2023-05-20 14:57:43 +0530

    Update couple.py

--- MukeshRobot/modules/couple.py
+++ MukeshRobot/modules/couple.py
@@ -80,7 +80,7 @@ Choose couples in your chat
 
  ❍ /couple *:* Choose 2 users and send their name as couples in your chat.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [CHERRY](https://t.me/DRACULA_CHEERY)............☆
 """
 
 __mod_name__ = "♨️Cᴏᴜᴘʟᴇ♨️"
