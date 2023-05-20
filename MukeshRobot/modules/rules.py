b5c6cd283abc90e17b188604a792f8e1846d95bd
Cɦεɾɾy <133900927+ITSS-CHEREY@users.noreply.github.com>
2023-05-20 13:21:08 +0530

    Update rules.py

--- MukeshRobot/modules/rules.py
+++ MukeshRobot/modules/rules.py
@@ -160,7 +160,7 @@ __help__ = """
  ‣ `/setrules <your rules here>`*:* set the rules for this chat.
  ‣ `/clearrules`*:* clear the rules for this chat.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [CHERRY](https://t.me/DRACULA_CHEERY)............☆
 """
 
 __mod_name__ = "⚡Rᴜʟᴇs⚡"
