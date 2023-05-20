f1dc4f72f5880e3e2678cde97ed8d9196e999eca
Cɦεɾɾy <133900927+ITSS-CHEREY@users.noreply.github.com>
2023-05-20 12:59:29 +0530

    Update video.py

--- MukeshRobot/modules/video.py
+++ MukeshRobot/modules/video.py
@@ -110,4 +110,4 @@ __help_ = """
 /video to download video song
 /vsong to download video song 
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆"""
+☆............𝙱𝚈 » [CHERRY](https://t.me/DRACULA_CHEERY)............☆"""
